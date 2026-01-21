59bd1faad82bd02a43a8eacd4dd4507e919b5e0c
Hamed Ansarian <hamed.ansarian@hotmail.com>
2026-01-21 16:08:18 +0000

    feat: enhance distributed lock service with Redis integration and configuration updates

--- src/DistributedLockManager/Configuration.cs
+++ src/DistributedLockManager/Configuration.cs
@@ -1,6 +1,11 @@
 ﻿using DistributedLockManager.Interfaces;
 using DistributedLockManager.Services;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using RedLockNet;
+using RedLockNet.SERedis;
+using RedLockNet.SERedis.Configuration;
+using StackExchange.Redis;
 
 namespace DistributedLockManager;
 
@@ -8,6 +13,14 @@ public static class CacheManagerConfiguration
 {
     public static void AddDistributedLockManager(this IServiceCollection services)
     {
+        services.AddSingleton<IDistributedLockFactory, RedLockFactory>(sp =>
+        {
+            var connectionMultiplexer = sp.GetRequiredService<IConnectionMultiplexer>();
+            var loggerFactory = sp.GetRequiredService<ILoggerFactory>();
+
+            return RedLockFactory.Create([new RedLockMultiplexer(connectionMultiplexer)], loggerFactory);
+        });
+
         services.AddScoped<IDistributedLockService, DistributedLockService>();
         services.AddScoped(typeof(IDistributedLockService<>), typeof(DistributedLockService<>));
     }
